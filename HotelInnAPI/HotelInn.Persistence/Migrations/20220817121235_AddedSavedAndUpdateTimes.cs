aaa8fb991490ee4b92b66164b9dd30307e646fe7
Uzair Aleem <uzairaleem1995@gmail.com>
2022-08-17 17:13:32 +0500

    Added migration to save last update and save times

---
+++ HotelInnAPI/HotelInn.Persistence/Migrations/20220817121235_AddedSavedAndUpdateTimes.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelInn.Persistence.Migrations
+{
+    public partial class AddedSavedAndUpdateTimes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdateTime",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SaveDateTime",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdateTime",
+                table: "UserReviews",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SaveDateTime",
+                table: "UserReviews",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdateTime",
+                table: "Hotels",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SaveDateTime",
+                table: "Hotels",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdateTime",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "SaveDateTime",
+                table: "Bookings",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUpdateTime",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SaveDateTime",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdateTime",
+                table: "UserReviews");
+
+            migrationBuilder.DropColumn(
+                name: "SaveDateTime",
+                table: "UserReviews");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdateTime",
+                table: "Hotels");
+
+            migrationBuilder.DropColumn(
+                name: "SaveDateTime",
+                table: "Hotels");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdateTime",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "SaveDateTime",
+                table: "Bookings");
+        }
+    }
+}
