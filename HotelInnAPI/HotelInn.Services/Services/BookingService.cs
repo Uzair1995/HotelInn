00308f5ace43a839a44ad8c8f9ba99ff92babc13
Uzair Aleem <uzairaleem1995@gmail.com>
2022-08-17 18:00:54 +0500

    Added code for placing a booking

--- HotelInnAPI/HotelInn.Services/Services/BookingService.cs
+++ HotelInnAPI/HotelInn.Services/Services/BookingService.cs
@@ -1,19 +1,53 @@
 ﻿using HotelInn.Contracts.Booking;
+using HotelInn.Domain.IRepositories;
 using HotelInn.Services.Abstractions;
+using System;
 using System.Threading.Tasks;
 
 namespace HotelInn.Services.Services
 {
     public class BookingService : IBookingService
     {
-        public BookingService()
-        {
+        private readonly Lazy<IBookingRepository> bookingRepository;
+        private readonly Lazy<IHotelRepository> hotelRepository;
+        private readonly Lazy<IUserRepository> userRepository;
 
+        public BookingService(
+            Lazy<IBookingRepository> bookingRepository,
+            Lazy<IHotelRepository> hotelRepository,
+            Lazy<IUserRepository> userRepository)
+        {
+            this.bookingRepository = bookingRepository;
+            this.hotelRepository = hotelRepository;
+            this.userRepository = userRepository;
         }
 
-        public Task<string> AddNewBookingAsync(NewBooking newBooking)
+        public async Task<string> AddNewBookingAsync(NewBooking newBooking)
         {
-            
+            if (newBooking == null)
+                return "Value cannot be null";
+
+            if (newBooking.CheckinDateTime >= newBooking.CheckoutDateTime || newBooking.CheckinDateTime <= DateTime.Now)
+                return "Invalid checkin checkout time provided!";
+
+            Domain.Models.Hotel hotel = await hotelRepository.Value.FindHotelAsync(newBooking.HotelId);
+            if (hotel == null)
+                return "Hotel ID does not match any records!";
+
+            Domain.Models.User user = await userRepository.Value.FindUserAsync(newBooking.UserId);
+            if (user == null)
+                return "User ID does not match any records!";
+
+            Domain.Models.Booking booking = new Domain.Models.Booking
+            {
+                HotelId = newBooking.HotelId,
+                UserId = newBooking.UserId,
+                CheckinDateTime = newBooking.CheckinDateTime,
+                CheckoutDateTime = newBooking.CheckoutDateTime
+            };
+
+            await bookingRepository.Value.AddOrUpdateBookingAsync(booking);
+            return "Saved successfully!";
         }
     }
 }
